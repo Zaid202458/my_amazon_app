a639f2f4acd5fce8a16b753f01c7995c1890ca7a
zaid202458 <zaid202458@gmail.com>
2024-11-30 01:15:08 +0300

    Add Config

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 
 
+//ِAPI
 const express =require("express");
 
 const PORT=3000;
 
 const app =express();
 
+app.get("/Yemen-devs",(req,res)=>{
+res.send("Welcome to Yemen Devs")
+});
+
+
 app.listen(PORT,"0.0.0.0",function(){
-    console.log("Done");
+    console.log(`Connected o Port: ${PORT}`);
 })
